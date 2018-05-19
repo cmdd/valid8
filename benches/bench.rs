9ce8dc31a66f61e2e4b2d2107d4e509d83547c2a
David Cao <dcao@protonmail.com>
2018-05-19 00:39:15 -0700

    improved benchmarks which show throughput

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,20 +2,24 @@
 extern crate criterion;
 extern crate valid8;
 
-use criterion::{Criterion, Fun};
+use criterion::{Criterion, Fun, ParameterizedBenchmark, Throughput};
 use valid8::*;
 
 fn validate_ascii(c: &mut Criterion) {
-    let manual  = Fun::new("validate", |b, i| b.iter(|| ascii::validate(*i)));
-    let faster  = Fun::new("faster", |b, i| b.iter(|| ascii::faster(*i)));
-    let suparch = Fun::new("super_arch", |b, i| b.iter(|| ascii::super_arch(*i)));
-    let arch    = Fun::new("arch", |b, i| b.iter(|| ascii::arch(*i)));
-    let default = Fun::new("default", |b, i: &&[u8]| b.iter(|| (*i).is_ascii()));
-
-    let s = include_bytes!("data/ascii1");
+    let s1 = include_bytes!("data/ascii1");
+    let params = vec![s1];
 
     let fs = vec![manual, faster, arch, suparch, default];
-    c.bench_functions("ascii", fs, s);
+    c.bench(
+        "ascii",
+        fs,
+        ParameterizedBenchmark::new("validate", |b, i| b.iter(|| ascii::validate(*i)), params)
+            .with_function("faster", |b, i| b.iter(|| ascii::faster(*i)))
+            .with_function("super_arch", |b, i| b.iter(|| ascii::super_arch(*i)))
+            .with_function("arch", |b, i| b.iter(|| ascii::arch(*i)))
+            .with_function("default", |b, i: &&[u8]| b.iter(|| (*i).is_ascii()))
+            .throughput(|s| Throughput::Bytes(s.len() as u32)),
+    );
 }
 
 criterion_group!(benches, validate_ascii);
